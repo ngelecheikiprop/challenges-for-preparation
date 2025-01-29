9c2d046637735d5713e47c5e3b0a57fc69ac67b9
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-01-29 16:37:25 +0300

    trial 1 challenge on tree constructor

---
+++ 7-challenge.py
@@ -0,0 +1,11 @@
+def TreeConstructor(strArr):
+  for i in range(10):
+    count = 0
+    for item in strArr:
+      if str(i) in item.split(',')[1]:
+        count += 1
+    if count > 2:
+      return 'false'
+  return 'true'
+
+print(TreeConstructor(input()))
