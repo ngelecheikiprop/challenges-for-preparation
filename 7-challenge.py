a2c2854696e2df8338b4b8936c24a0c33ae8fffa
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-01-29 20:33:35 +0300

    constructor another trial

--- 7-challenge.py
+++ 7-challenge.py
@@ -3,17 +3,17 @@ def TreeConstructor(strArr):
   for i in range(10):
     child_count = 0
     for item in strArr:
-      if str(i) in item.split(',')[1]:
+      if str(i) == item.split(',')[1][:-1]:
         child_count += 1
     if child_count > 2:
       return 'false'
   for i in range(10):
     parent_count = 0
     for item in strArr:
-      if str(i) in item.split(',')[0]:
+      if str(i) == item.split(',')[0][1:]:
         parent_count += 1
     if parent_count > 1:
-      return 'false'      
+      return 'false'
   return 'true'
 
 
